831b3617fcd5eef7a30dda40a373515d6c9a67fd
whg7251 <49737277+whg7251@users.noreply.github.com>
2019-11-28 10:15:56 +0800

    创建常量类 add  客户端退出标识常量

---
+++ src/main/java/com/basics/socket1/Constants.java
@@ -0,0 +1,12 @@
+package com.basics.socket1;
+
+/**
+ * @ClassName Constants
+ * @Description 常量类
+ * @Author Guo
+ * @Date 2019/11/28 9:12
+ * @Version 1.0
+ **/
+public class Constants {
+    static final String QUIT = "quit";
+}
